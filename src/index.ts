d1b5c5a087cb1663d770e42d3b3ad746dce81604
no_names <vadim.atamanyuk1305.va@gmail.com>
2026-05-25 15:32:58 +0300

    feat: add sky-service-ui-components and NotificationElement documentation

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import './styles/scrollbar.css';
 export * from './shared/ui';
 export * from './features';
 export * from './sdk';
+export * from 'sky-service-ui-components';
+export * from 'sky-service-ui-components/modules';
